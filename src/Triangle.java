c6ad052863ef63efc6ebcff6a7fcc3c7bc8f5276
Josh Pike <joshuaadam.pike@calbaptist.edu>
2019-03-28 17:41:57 -0700

    Bugged Version 1.0

--- src/Triangle.java
+++ src/Triangle.java
@@ -35,9 +35,9 @@ public class Triangle
     //******************************************************************
     public boolean isIsosceles() {
         if (
-                ( (s1 == s2) && (s1 != s3) ) ||
-                ( (s1 == s3) && (s1 != s2) ) ||
-                ( (s2 == s3) && (s2 != s1) ) ) {
+                ( (s1 == s2) || (s1 != s3) ) ||
+                ( (s1 == s3) || (s1 != s2) ) ||
+                ( (s2 == s3) || (s2 != s1) ) ) {
             isIsosceles = true;
             System.out.println("The triangle is ISOSCELES!");
         }
